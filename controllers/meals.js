7480773812e50097aed098be2f8a5a00c137bf65
Mathew <muckele7@gmail.com>
2024-01-18 21:18:22 -0500

    creates create functionality

--- controllers/meals.js
+++ controllers/meals.js
@@ -1,5 +1,31 @@
 import { Meal } from "../models/meal.js"
 
 function newMeal(req, res) {
-  
+  Meal.find({}).sort('meal')
+  .then(meals => {
+    res.render('meals/new', {
+      meals,
+      title: 'Add Meal'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/movies')
+  })
+}
+
+function create(req, res) {
+  Meal.create(req.body)
+  .then(meal => {
+    res.redirect('/meals/new')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/meals/new')
+  })
+}
+
+export {
+  newMeal as new,
+  create
 }
--- routes/meals.js
+++ routes/meals.js
@@ -3,6 +3,8 @@ import * as mealsCtrl from '../controllers/meals.js'
 
 const router = Router()
 
+router.get('/new', mealsCtrl.new)
+router.post('/, mealsCtrl.create')
 
 
 
